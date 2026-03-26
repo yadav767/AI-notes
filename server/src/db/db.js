25fa469f1d230601da0b897843918a8ba163d9d2
Ayush Yadav <yyadavaayush767@gmail.com>
2026-03-26 14:01:58 +0530

    Update db.js

--- server/src/db/db.js
+++ server/src/db/db.js
@@ -1,34 +1,11 @@
 const mongoose = require("mongoose");
-let cached = global.mongoose || (global.mongoose = { conn: null, promise: null });
 
 async function connectDB() {
-  // Check karo connection alive hai ya nahi
-  if (cached.conn && mongoose.connection.readyState === 1) {
-    console.log("Reusing existing DB connection");
-    return cached.conn;
-  }
-
-  // Reset karo agar connection dead hai
-  cached.conn = null;
-  cached.promise = null;
-
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(process.env.MONGODB_URL, {
-      bufferCommands: false,
-      maxPoolSize: 10,
-      serverSelectionTimeoutMS: 10000, // 10 sec timeout
-      socketTimeoutMS: 45000,
-    });
-  }
-  try {
-    cached.conn = await cached.promise;
-    console.log("Connected to the DB");
-  } catch (error) {
-    cached.promise = null;
-    console.log(error);
-    throw error;
-  }
-  return cached.conn;
+    try {
+        await mongoose.connect(config.MONGODB_URL)
+        console.log("Connected to DB...");
+    } catch (error) {
+        console.log("Failed to connect DB !", error);
+    }
 }
-
 module.exports = connectDB;
