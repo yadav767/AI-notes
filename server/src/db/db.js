e9e3640044f0c28ee7bc259b174b621b4af0a422
Ayush Yadav <yyadavaayush767@gmail.com>
2026-03-26 14:04:41 +0530

    Update MongoDB connection URL to use environment variable

--- server/src/db/db.js
+++ server/src/db/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 async function connectDB() {
     try {
-        await mongoose.connect(config.MONGODB_URL)
+        await mongoose.connect(process.env.MONGODB_URL)
         console.log("Connected to DB...");
     } catch (error) {
         console.log("Failed to connect DB !", error);
